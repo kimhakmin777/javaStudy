a77276287cdb4c86db548e11da4d98c70ce30810
kimhakmin777 <a98171909@gmail.com>
2022-05-06 00:07:30 +0900

    Hellowolrd

---
+++ src/Helloworld.java
@@ -0,0 +1,95 @@
+public class Helloworld {
+    public static void main(String[]args){
+        add();
+        b();
+        c();
+        d();
+    }
+    public static void add(){
+        int a=5;
+        System.out.println(a+a);
+    }
+
+    public static void b() {
+        int a = 5;
+        System.out.println(a - a);
+    }
+
+    public static void c() {
+        int a = 5;
+        System.out.println(a * a);
+    }
+
+    public static void d() {
+        int a = 5;
+        System.out.println(a / a);
+    }
+}
+    /*public static void main(String[]args){
+  //기본
+        System.out.println("Hello World");
+        System.out.println(36);
+        System.out.println(37+32-30);
+        System.out.println("hi"+"hi");
+        int a=10;
+        System.out.printf("%d",a);
+
+        System.out.print(12);
+        System.out.print(12);
+        System.out.print(12);
+//타입
+        String str="문자열";
+        int i=1000;
+        double d=2.36;
+
+        System.out.println(str+i+d);
+//형변환
+        int b=123;
+        String st="23";
+        int c=Integer.parseInt(st);
+        System.out.println(c+b);
+    }
+    public static void Hello() {
+        System.out.println("Hello");
+
+    }*/
+
+
+//내용 정리
+//자바 실행할려면 jdk가 필요하다!!
+//자바는 객채지향 언어 중 하나다
+//기본적인 출력을 할때는 print,println,printf가 주로 사용 '따로 조사 하자'
+//자바의 변수 같은 경우는 형을 지정해줘야함 예로 string a="answk"
+//문자열 변환는 밑에 쓴거 참고 하셈;; 귀찮음 치기 '따로 조사 하자'
+//연산자 +더하기 -빼기 %몫 /나머지
+//메소드는 만들어 주는거 안에 내용이라고 생각하면 될듯 <클래스 안에 들어가는것> '따로 조사 하자'
+
+//기본 개념
+// 일단 변수는 소문자.
+// 클래스는 대문자로 사용
+//자바는 2학년에 배움 객체 지향 언어 중 하나임
+//객채 지향이란 자전거 제작시 부품들이 뭉쳐져서 하나가 되는 것을 다른 것으로도 활용 가능해지게 가져와서 사용하는것임
+// 중요한것은 ;(세미클론) 무조건 마지막에 붙이기!!
+
+//프린트 종류
+// sout 사용시 System.out.println(); 바로 나옴
+//System.out.println(); 안에서는 문자, 숫자, 연산, 문자 연산 등이 가능
+// print ln은 세로 나열, f는 그냥 이해해, 뒤에 뭐 없으면 가로 나열
+
+//주석
+//는 한줄 주석
+/* 은 해당 범위 까지*/
+// ctrl+//는 드래그 한만큼 주석
+// string 첫번째 문자열 "문자열"
+
+//타입
+// int = integer 정수 숫자
+// double= 소수 실수
+// 변수할때 + 하면 같이 나오는 듯 아마
+
+// 형 변환
+//Integer. 하고 parse__이나 to__하면 예로 인트에서 스트링으로 바뀌는 식으로 됨.
+//앞에 바꿀 예로 String c= 요런식으로 하고 Integer. 응용하여 전개
+
+//메소드 중요!![이거 할려구 자바한다고 생각]
+// 첫 타이틀을 클래스라 칭하고, 그 {}안에 포함되있는 것을 메소드라고 칭하면 됨 ㅎ
